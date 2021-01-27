1902e1a53944caecb67211d066e6c47bd248123d
Nicole Remy <nicoleremy95@gmail.com>
2021-01-27 11:44:53 -0800

    Add Resume, Aboutme, and Projects pages to Router and Landing page buttons, Add styling to buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Appbar from './components/Appbar';
 import Landing from './pages/Landing';
+import Aboutme from './pages/Aboutme';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      <Router>
+        <Switch>
+          <Route exact path = '/'>
+            <Landing/>
+          </Route>
+          <Route exact path = '/about-me'>
+            <Aboutme/>
+          </Route>
+          <Route exact path = '/projects'>
+            <Projects/>
+          </Route>
+          <Route exact path = '/resume'>
+            <Resume/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import {Button, Grid} from '@material-ui/core/';
-import { Link } from 'react-scroll';
+import { Link } from 'react-router-dom';
 
 
 const useStyles = makeStyles((Theme)=>
@@ -9,17 +9,18 @@ const useStyles = makeStyles((Theme)=>
         root: {
             flexGrow: 1,
           },
-        // buttons: {
-        //     display: 'flex',
-        //     justifyContent: 'center'
-        // },
+        link: {
+            textDecoration: "none",
+        },
         buttonOutline:{
             borderStyle: 'solid',
             borderWidth: '20',
             borderColor: 'white'
         },
         buttonText: {
-            margin: 20
+            margin: 20,
+            color: "white !important"
+
         }
     })
 )
@@ -30,25 +31,27 @@ export default function Buttons() {
             <Grid container >
                 <Grid item xs={1} sm={1} md={3} lg={4} direction="column"></Grid>
                 <Grid item xs={10} sm={8} md={6} lg={4} direction="column" >
-                    <Link to="about-me">
+                    <Link to='/projects' className={classes.link}>
                         <Button >
                             <div className={classes.buttonOutline}>
                                 <p className={classes.buttonText}>PROJECTS</p>
                             </div>
                         </Button>
                     </Link>
-                    
-                    <Button >
-                        <div className={classes.buttonOutline}>
-                            <p className={classes.buttonText}>ABOUT ME</p>
-                        </div>
-                    </Button>
-                    <Button to="resume">
-                        <div className={classes.buttonOutline}>
-                            <p className={classes.buttonText}>RESUME</p>
-                        </div>
-                    </Button>
-           
+                    <Link to='/about-me' className={classes.link}>
+                        <Button >
+                            <div className={classes.buttonOutline}>
+                                <p className={classes.buttonText}>ABOUT ME</p>
+                            </div>
+                        </Button>
+                    </Link>
+                    <Link to='/resume' className={classes.link}>
+                        <Button >
+                            <div className={classes.buttonOutline}>
+                                <p className={classes.buttonText}>RESUME</p>
+                            </div>
+                        </Button>
+                    </Link>
                 </Grid>
             </Grid> 
             
